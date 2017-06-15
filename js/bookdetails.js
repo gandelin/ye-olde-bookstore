683a9a9e68415761be928181867e90cca8c75b10
Gary Andelin <gary.l.andelin@boeing.com>
2017-06-15 14:49:22 -0700

    Added cart logic

--- js/bookdetails.js
+++ js/bookdetails.js
@@ -2,20 +2,27 @@
 (function() {
   'use strict;'
   
-var BOOKS_PERSISTENCE_KEY = 'bookstorage';  
+var BOOKS_PERSISTENCE_KEY = 'bookstorage';
+var CART_PERSISTENCE_KEY = 'shoppingcartstorage'; 
+  
 var books = getBooks();
-var bookDetailsTemplate = Handlebars.compile($('#bookDetailsTemplate').html());
+var selectedBook = {};
+var cart = getCart();
 
+var bookDetailsTemplate = Handlebars.compile($('#bookDetailsTemplate').html());
+  
 window.onload = function displayBookDetails(evt) {
   evt.preventDefault();
   var url = evt.target.URL; 
   var id = url.substr(url.indexOf('?id=')+4);
-
+  selectedBook = books[id];
+  
   var data = {
-    book: books[id]
+    book: selectedBook
   }
   var html = bookDetailsTemplate(data);
   $('#bookDetails').html(html);
+  $('#addToCart').on('click', addBookToCart);
 }
   
 function getBooks() {
@@ -26,6 +33,47 @@ function getBooks() {
   else {
     return [];
   }  
+}
+  
+function addBookToCart(evt) {
+  evt.preventDefault();
+  var order = new Order(selectedBook);
+  cart.orders.push(order);
+  saveCart(cart);
+}
+
+function getCart() {
+  var c = localStorage[CART_PERSISTENCE_KEY];
+  
+  if (c !== "undefined") {
+    return JSON.parse(c);
+  }
+  else {
+    return new Cart();
+  }  
+}
+
+function initializeCart() {
+  cart.orders = [];
+}
+  
+function saveCart(cart) {
+  localStorage[CART_PERSISTENCE_KEY] = JSON.stringify(cart);
+}
+
+function Order(book) {
+  this.title = book.title,
+  this.price = book.price,
+  this.quantity = 1,
+  this.totalPrice = book.price
+}
+  
+function Cart() {
+  this.orders = [];
+  this.orderPrice = 0;
+  this.tax = 0;
+  this.shipping = 0;
+  this.totalPrice = 0;
 }
   
 })();
---
+++ js/shoppingcart.js
@@ -0,0 +1,87 @@
+(function() {
+  'use strict;'
+  
+  var CART_PERSISTENCE_KEY = 'shoppingcartstorage'; 
+  
+  var cart = getCart();
+  
+  var shoppingCartTemplate = Handlebars.compile($('#shoppingCartTemplate').html());
+  
+  window.onload = function displayCart() {
+    var data = {
+      cart: cart
+    }
+    var html = shoppingCartTemplate(data);
+    $('#shoppingCartDiv').html(html);
+    $('#placeOrder').on('click', placeOrder);
+  }
+  
+  function placeOrder() {
+    alert("Thank you for your order")
+    clearCart();
+  }
+  
+  function getCart() {
+    var c = localStorage[CART_PERSISTENCE_KEY];
+    var myCart = new Cart();
+    if (c !== "undefined") {
+      myCart = JSON.parse(c);
+    }
+    return updateOrderTotals(myCart);
+  }
+  
+  function updateOrderTotals(cart) {
+    var myCart = new Cart();
+    var orderPrice = 0.0;
+    
+    for (var ix = 0; ix < cart.orders.length; ix++) {
+      var origOrder = cart.orders[ix];
+      orderPrice += parseFloat(origOrder.totalPrice);
+      var order = new Order();
+      order.title = origOrder.title;
+      order.price = parseFloat(origOrder.price);
+      order.quantity = parseInt(origOrder.quantity);
+      order.totalPrice = orderPrice;
+      myCart.orders.push(order);
+    }
+    myCart.orderPrice = orderPrice;
+    myCart.tax = parseFloat((0.09 * orderPrice).toFixed(2));
+    myCart.shipping = 5.0;
+    myCart.totalPrice = (orderPrice + myCart.tax + myCart.shipping).toFixed(2);
+    
+    return myCart;
+  }
+  
+
+  function clearCart() {
+    cart = new Cart();
+    saveCart();
+  }
+  
+  function saveCart() {
+    localStorage[CART_PERSISTENCE_KEY] = JSON.stringify(cart);
+  }
+  
+  function Order(book) {
+    this.title = book.title;
+    this.price = book.price;
+    this.quantity += 1;
+    this.totalPrice = book.price;
+  }
+
+  function Order() {
+    this.title = "";
+    this.price = 0.0
+    this.quantity = 0;
+    this.totalPrice = 0.0;
+  }
+  
+  function Cart() {
+    this.orders = [];
+    this.orderPrice = 0;
+    this.tax = 0;
+    this.shipping = 0;
+    this.totalPrice = 0;
+  }
+  
+})();
